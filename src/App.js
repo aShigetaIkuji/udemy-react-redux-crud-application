862548030a0cb3d6ab1d2719c4b8830d41791885
aShigetaIkuji <shigeta.ikuji@access-company.com>
2018-09-05 19:21:13 +0900

    Learn state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0}
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count +1 })
+  }
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count -1 })
+  }
+
   render() {
     return (
       <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {console.log("clicked!")}} />
+        <div>count: { this.state.count }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
       </React.Fragment>
     )
   }
